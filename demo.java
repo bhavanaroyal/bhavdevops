dcb61e10e2109e335f1a99faabdaba3d0c916aa8
bhavdevops <yelem255@gmail.com>
2019-06-27 07:47:12 +0000

    code merged

--- demo.java
+++ demo.java
@@ -1,3 +1,36 @@
+void add()
+{
+ c=a+b;
+}
+void sub()
+{
+ c=a-b;
+}
+void mul()
+{
+ c=a*b;
+}
+void div()
+{
+ c=a/b;
+}
+
+
+
+
+
+
+
+
+
+
+
+void mastercode()
+{
+......code....
+}
+
+
 void ()
 {
 .....demo code....
