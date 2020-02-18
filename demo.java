bc2f5298622b415dab99263827967b12015a4c8a
bhavdevops <yelem255@gmail.com>
2020-02-18 05:27:03 +0000

    democlass

--- demo.java
+++ demo.java
@@ -1,37 +1,4 @@
-void add()
+class demo
 {
- c=a+b;
-}
-void sub()
-{
- c=a-b;
-}
-void mul()
-{
- c=a*b;
-}
-void div()
-{
- c=a/b;
-}
-
-
-
-
-
-
-
-
-
-
-
-void mastercode()
-{
-......code....
-}
-
-
-void ()
-{
-.....demo code....
+ print('hello');
 }
